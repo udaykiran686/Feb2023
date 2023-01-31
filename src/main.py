55912c94c93aad899d2e74f884b951eb8fd4d9cc
udaykiran686 <udaykiran686@gmail.com>
2023-01-31 14:35:00 +0530

    rel_1.0 changes commited

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
 ### this is for testing only
 # added requirements.txt
-## branches are created rel_1.0  branches are created rel_1.1
+## branches are created rel_1.0  branches are created rel_1.0
+# again checking
